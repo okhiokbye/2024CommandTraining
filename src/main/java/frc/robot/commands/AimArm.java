3e892d625788d8e632a2727f1dbb8c7a49065ddc
okhiokbye <125434547+okhiokbye@users.noreply.github.com>
2024-09-26 14:51:08 -0700

    armcommand

--- src/main/java/frc/robot/commands/AimArm.java
+++ src/main/java/frc/robot/commands/AimArm.java
@@ -3,9 +3,23 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Arm;
 
-public class AimArm extends PIDCommand {
+public class AimArm extends Command {
     private final Arm m_arm;
-    private final <D>
+    private final double setpoint;
+    public AimArm(Arm arm, double setpoint){
+        this.m_arm = arm;
+        this.setpoint = setpoint;
+    }
+    @Override
+    public void initialize() {
+        m_arm.setSetpoint(m_setpoint);
+        m_arm.enable();
+  }
 
+  // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() {
+        return m_arm.getController().atSetpoint();
+  }
 
 }
