23776034d007bad9e77a3b635ecebfda2df7ad22
okhiokbye <125434547+okhiokbye@users.noreply.github.com>
2024-10-07 08:36:58 -0700

    lol

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-// Things to do: telemetry,intake stop unjank, shoot varying speed??
+// Things to do: telemetry,intake stop unjank, shoot varying speed?? LIMELIGHTTTTTT PLEASE WORK
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
